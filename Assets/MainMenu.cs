05c9788cf50a384ef48c91f4ef13db36dcb1b8d8
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2018-11-05 14:37:33 +0530

    Init App

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public float factorValue = 3.0f;
+	public AudioSource mainAudioSource;
+
+    public void playGame() {
+
+		StartCoroutine (this.fade());
+
+    }
+
+	public void quitGame() {
+		Debug.Log ("Quit!");
+		Application.Quit ();
+	}
+		
+	public IEnumerator fade(){
+		while(this.mainAudioSource.volume > 0.0f){
+			this.mainAudioSource.volume -= Time.deltaTime * this.factorValue;
+			yield return new WaitForSeconds (0.1f);
+		}
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
---
+++ Assets/masterVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class masterVolume : MonoBehaviour {
+
+	public GameObject sourceObject;
+	public Slider mySlider;
+
+	[HideInInspector]
+	public AudioSource masterAudioSource;
+
+	void Start () {
+		this.masterAudioSource = sourceObject.GetComponent<AudioSource>();
+	}
+	
+	public void myVolume(float vol){
+		this.masterAudioSource.volume = mySlider.value;
+	}
+}
